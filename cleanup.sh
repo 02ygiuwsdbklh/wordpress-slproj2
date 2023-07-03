dd631d786adb972c806d06ef09d142ed8cb0233e
noobx2empt1L <8340935+noobx7wt2fDL@users.noreply.github.com>
2023-07-03 12:39:44 +0530

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -5,7 +5,7 @@ docker rm $(docker ps -a -q)
 sleep 2
 docker rmi wordpress:6.2
 sleep 2
-docker rmi mariadb:11.0.2
+docker rmi mariadb:10.6.4-focal
 # remove volumes
 docker-compose down --volumes
 sleep 2
